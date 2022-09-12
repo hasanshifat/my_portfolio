8f5ec65e54ad9dc384c46f75a0dcd626be8bd96a
Mahmudul Hasan Shifat <shifatmad99@gmail.com>
2022-09-12 17:10:39 +0600

    commit 2nd

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const colorBlack = Color(0xff181818);
+  static const colorRed = Color(0xffD2001A);
+  static const colorYellow = Color(0xffFEDB39);
+  static const colorPink = Color(0xffD61C4E);
+  static const colorGreyDark = Color(0xff413F42);
+  static const colorWhite = Colors.white;
+}
---
+++ lib/view/site_layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../Large_Screen/large_screen.dart';
+import '../helpers/responsiveness.dart';
+import '../widgets/Custom_Text/top_nav.dart';
+
+class SiteLayout extends StatelessWidget {
+  final GlobalKey<ScaffoldState> sckey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: sckey,
+      appBar: topNavBar(context, sckey),
+      drawer: Drawer(
+          //  child: SideMenu(),
+          ),
+      body: ResponsiveWidget(
+        largeScreen: LargeScreen(),
+        smallScreen: Padding(
+          padding: const EdgeInsets.all(8.0),
+          // child: localNavigator(),
+        ),
+      ),
+    );
+  }
+}
